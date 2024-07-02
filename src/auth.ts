9f613cc31717357704dca61e1b767ef6f8253cf8
subh <harvirbuttar2917@gmail.com>
2024-07-02 10:51:55 -0700

    init

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,9 @@
 import NextAuth from "next-auth";
 import googleProvider from "next-auth/providers/google";
 import credentialsProvider from "next-auth/providers/credentials";
+import { CredentialsSignin } from "next-auth";
+import { User } from "./models/userModel";
+import bcryptjs, {compare} from 'bcryptjs'
 
 export const {handlers, signIn, signOut, auth} = NextAuth({
 
@@ -18,12 +21,55 @@ export const {handlers, signIn, signOut, auth} = NextAuth({
         // we could also use default `auth` form but we will use our custom form
         credentialsProvider({
             name: 'Credentionals',
+
+            // what are needed!
             credentials: {
                 email: {
                     label: 'Email',
                     type: 'email'
+                },
+                password: {
+                    label: 'Password',
+                    type: 'password'
                 }
+            },
+
+            // So after user has gave the email and pasword no we can verify all info in authorize method.
+
+            async authorize(credentials) {
+
+                const {email, password} = credentials;
+
+                if (!email || !password){
+                    throw new CredentialsSignin("Please provide email and password")
+                }
+
+
+                // notice we can't a user object having password in it!
+                const user = await User.findOne({
+                    email
+                }).select('+password');
+                
+                if (!user) {
+                    throw new CredentialsSignin("User not found!")
+                }
+
+                if (!user.password){
+                    throw new CredentialsSignin("Invalid email or password")
+                }
+
+                const isMatch = await compare(password as string, user.password);
+
+                if (!isMatch){
+                    throw new CredentialsSignin("Invalid email or password")
+                }
+
+                if (password !== 'passcode')
+                    throw new CredentialsSignin(undefined, {cause: 'PPassword does not match',});
+                
+                return {name: user.name, email: user.email, id: user._id};
             }
+
         })
     ]
 });
--- src/note.ts
+++ src/note.ts
@@ -14,14 +14,25 @@
 *       i) export const {handlers, signIn, signOut, auth} = NextAuth({...})
 *       ii) Brief :-
 *           - handlers will be used in api's because it hold GET & POST
-*           - auth will be used as middleware 
+*           - auth will be used as middleware  
 * 
 *   [c] # src/app/api/auth/[...nextauth]/route.ts
 *       i) export {GET, POST} = handlers
 * 
 *   [d] #src/middleware
 *       i) export {auth as middleware}
+* 
+* DATABASE :-
+*   [a] npm install mongoose (Object Data Modeling)
+*   
+*   [b] Setup User or other models in /src/models/userModels
+* 
+*   [c] npm i bcryptjs (bcrypt is built using C++ and bcryptjs using JS)
+* 
+* UTILS :-
+*   [a] Shadcn-ui
+*       - npx shadcn-ui@latest init
+*       - npx shadcn-ui@latest add Card
+*       - npx shadcn-ui@latest Input
 *       
-* 
-* 
  */
